380987f6f102aaf967a50319eee31f3c4a229b2f
smathew54 <88407507+smathew54@users.noreply.github.com>
2025-05-29 21:20:23 -0500

    adding the files for the console

---
+++ index.js
@@ -0,0 +1,24 @@
+let code1;
+console.log(code1);
+
+code1 = 7 + 3;
+console.log(code1);
+
+let code2 = 160 / 2 - 40;
+console.log(code2);
+
+let code3 = (15 % 4) + 38;
+console.log(code3);
+
+let message = "The vault has been secured. The combination is:";
+console.log(message);
+
+let codeA = code1 + code2 + code3;
+console.log(codeA);
+
+let codeB = `${code1}-${code2}-${code3}`;
+console.log(codeB);
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
